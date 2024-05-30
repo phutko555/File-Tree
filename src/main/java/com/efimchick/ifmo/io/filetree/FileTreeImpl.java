d353f7390747942daa3401a59edf001dddcdf863
Giorgi Phutkaradze <phutkaradzeg3@gmail.com>
2024-05-30 17:05:19 +0400

    ddd

--- src/main/java/com/efimchick/ifmo/io/filetree/FileTreeImpl.java
+++ src/main/java/com/efimchick/ifmo/io/filetree/FileTreeImpl.java
@@ -11,7 +11,6 @@ public class FileTreeImpl implements FileTree {
     public Optional<String> tree(Path path) {
         if (path == null || !Files.exists(path))
             return Optional.empty();
-
         try {
             return Optional.of(generateTree(path, "", true));
         } catch (IOException e) {
